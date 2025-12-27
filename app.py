01e9acbd05b739116da3c37555fa8ebccace3460
18shubham <18shubham@gmail.com>
2025-12-27 14:45:53 +0400

    test py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Production-Ready Calculator is Live!"
+    return "Its Broken!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
